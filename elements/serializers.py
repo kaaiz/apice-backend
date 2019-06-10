6e1da6a73a43bce09646ba35b99a05ec566740d7
Gabriel <manzur.g@gmail.com>
2019-06-10 00:46:27 -0400

    added apis for types and products

--- elements/serializers.py
+++ elements/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Element, Category
+from .models import Element, Category, Type, Product
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -9,4 +9,14 @@ class CategorySerializer(serializers.ModelSerializer):
 class ElementSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'title', 'text', 'image', 'category', 'display', 'created_date',)
+        model = Element
+
+class TypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'title', 'text', 'image', 'created_date',)
+        model = Category
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'title', 'text', 'image', 'category', 'display', 'price', 'stock', 'created_date',)
         model = Element
--- elements/urls.py
+++ elements/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import CategoryList, CategoryDetail, ElementList, ElementDetail
+from .views import CategoryList, CategoryDetail, ElementList, ElementDetail, TypeList, TypeDetail, ProductList, ProductDetail
 
 urlpatterns = [
-    path('element/<int:pk>/', ElementDetail.as_view()),
-    path('element/', ElementList.as_view()),
     path('category/<int:pk>/', CategoryDetail.as_view()),
     path('category/', CategoryList.as_view()),
+    path('element/<int:pk>/', ElementDetail.as_view()),
+    path('element/', ElementList.as_view()),
+    path('type/<int:pk>/', TypeDetail.as_view()),
+    path('type/', TypeList.as_view()),
+    path('product/<int:pk>/', ProductDetail.as_view()),
+    path('product/', ProductList.as_view()),
 ]
